23b6665da28bcc9fbc3ca399dc94f3e92a0be093
link857 <2761766788@qq.com>
2022-06-24 12:35:12 +0800

    fix a bug

--- server/api.py
+++ server/api.py
@@ -68,7 +68,7 @@ def heartbeat(data: HeartbeatIn):
     - msg: 返回值信息
     """
 
-    # data.loc.info = binascii.unhexlify(data.loc.info.encode('utf-8')).decode()
+    data.loc.info = binascii.unhexlify(data.loc.info.encode('utf-8')).decode()
 
     logger.debug(f"Recv heartbeat: {data}")
 
